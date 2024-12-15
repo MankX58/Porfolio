f57a75299bed197cd898509257ddf5977796bbb1
MankX58 <manuelguvi500@gmail.com>
2024-12-15 13:54:20 -0500

    asdas

--- certificates.js
+++ certificates.js
@@ -1,5 +1,5 @@
 export const certificates = [{
-    src: "https://pdf.ac/R7C9X"
+    src: "./public/docs/Certificado Excel Manuel Gutiérrez Villegas.pdf"
 },
 {
     src: "./public/docs/Diploma_Tailwind_Manuel.pdf"
--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Certificates(certificate) {
   return (
-    <div className="flex">
-      <object
+    <div className="flex flex-row gap-[1rem]">
+      {/* <object
         data={certificate.src}
         type="application/pdf"
         className="p-[1rem] min-w-[25rem] min-h-[23rem]"
@@ -11,7 +11,8 @@ export default function Certificates(certificate) {
         <p>
           alternate text <a href={certificate.src}>link</a>
         </p>
-      </object>
+      </object> */}
+      <iframe src={certificate.src} height="200" width="300" frameborder="0" className="m-[.5rem]"></iframe>
     </div>
   );
 }

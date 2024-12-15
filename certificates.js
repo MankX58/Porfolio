6b6abb36cd980fb92369abe66e420eba3d0e411b
MankX58 <manuelguvi500@gmail.com>
2024-12-15 13:28:05 -0500

    change certicates paths

--- certificates.js
+++ certificates.js
@@ -1,15 +1,15 @@
 export const certificates = [{
-    src: "/public/docs/Diploma_Javascript_Manuel.pdf"
+    src: "https://pdf.ac/R7C9X"
 },
 {
-    src: "/public/docs/Diploma_Tailwind_Manuel.pdf"
+    src: "./public/docs/Diploma_Tailwind_Manuel.pdf"
 },
 {
-    src: "/public/docs/Certificado Excel Manuel Gutiérrez Villegas.pdf"
+    src: "./public/docs/Certificado Excel Manuel Gutiérrez Villegas.pdf"
 },
 {
-    src: "/public/docs/Certificado_Programacion_Basica.pdf"
+    src: "./public/docs/Certificado_Programacion_Basica.pdf"
 },
 {
-    src: "/public/docs/Certificado_Programacion_Basica_2.pdf"
+    src: "./public/docs/Certificado_Programacion_Basica_2.pdf"
 }]

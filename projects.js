611a283d79773e55dd2805de95512a15d9b6afef
Manuel <manuelguvi500@gmail.com>
2025-04-19 21:05:08 -0500

    New project added

--- projects.js
+++ projects.js
@@ -1,27 +1,31 @@
-export const projects = [{
+export const projects = [
+  {
     repo: "https://github.com/MankX58/Cake-Shop",
     url: "https://cake-shop-beryl.vercel.app/",
     img: "https://i.ibb.co/XZHLnXP/CakeShop.png",
     title: "Cake Shop",
-    description: "Una tienda de pasteles funcional hecha con ReactJS y Tailwindcss",
-},
-{
+    description:
+      "Una tienda de pasteles funcional hecha con ReactJS y Tailwindcss",
+  },
+  {
     repo: "https://github.com/MankX58/Porfolio",
     url: "https://porfolio-mankx.vercel.app/",
     img: "https://i.ibb.co/y8nxmJM/image.png",
     title: "Portafolio",
-    description: "Portafolio hecho con ReactJS y Tailwindcss"
-},
-{
+    description: "Portafolio hecho con ReactJS y Tailwindcss",
+  },
+  {
     repo: "https://rieed39.vercel.app/",
     url: "https://rieed39.vercel.app/",
     img: "https://i.ibb.co/tYNB9bP/image.png",
     title: "RIEED39 PAGE",
-    description: "Pagina web hecha para mi proyecto RIEED39"},
-{
-    repo: "",
-    url: "",
-    img: "https://i.ibb.co/2q1r3cN/Portfolio.png",
-    title: "Portfolio",
-    description: "This portfolio was made using ReactJS and Tailwindcss"},
-]
+    description: "Pagina web hecha para mi proyecto RIEED39",
+  },
+  {
+    repo: "https://github.com/MankX58/calculadora-planeta",
+    url: "https://calculadora-planeta.vercel.app/",
+    img: "https://i.ibb.co/dJtWSXD3/image.png",
+    title: "Calculadora de peso",
+    description: "Calculadora de peso en diferentes planetas",
+  },
+];
